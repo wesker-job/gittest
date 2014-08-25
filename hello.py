13ce732b638fd2f4ed0119760fa136d9b68aa6b3
wesker-job <weiren0516@gmail.com>
2014-08-25 12:03:26 +0800

    py test2

--- hello.py
+++ hello.py
@@ -1,3 +1,23 @@
 #!/usr/bin/env python
 
 print("Hello", "World!")
+
+a = 5
+if a > 10:
+  print ("a > 10")
+elif a > 0:
+  print ("a > 0")
+else:
+  print ("a < 0")
+
+b = 0
+while b<2:
+  b = b + 1
+  print("b"+str(b))
+
+for sh in ["a","b","c"]:
+  print(sh)
+
+line = input("input value:")
+print(line)
+
